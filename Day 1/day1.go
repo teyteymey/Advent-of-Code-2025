7061803059a2476795e8d523f9bdb78b16fa96b0
teyteymey <tatiana.meyer.u@gmail.com>
2025-12-25 18:45:10 +0000

    finish codee

--- Day 1/day1.go
+++ Day 1/day1.go
@@ -4,7 +4,7 @@ import ("fmt"; "bufio"; "os"; "log"; "strconv")
 
 
 func main() {
-    file, err := os.Open("input2.txt")
+    file, err := os.Open("input.txt")
     if err != nil {
         log.Fatal(err)
     }
@@ -15,9 +15,9 @@ func main() {
     currentPosition := 50
     timesAtZero := 0
     for scanner.Scan() {
-        fmt.Println("---------")
+        // fmt.Println("---------")
         line := scanner.Text()
-        fmt.Println(line)
+        // fmt.Println(line)
 
         numberPart, err := strconv.Atoi(line[1:])
         if err != nil {
@@ -25,12 +25,19 @@ func main() {
         }
         // 5 L10 -5 100-5 = 95
         // 98 R3 101 100-101 = 1
+        //normalize if number is huge and its a lot of turns of the wheel
+        // fmt.Println("numberPart before and after")
+        // fmt.Println(numberPart)
+        //100 = +0 
+        numberPart = numberPart % 100
+        // fmt.Println(numberPart)
+
         if line[0] == 'L' {
             currentPosition -= numberPart
         } else {
             currentPosition += numberPart
         }
-        fmt.Println(currentPosition)
+        // fmt.Println(currentPosition)
 
         //normalize the results in the wheel
         if currentPosition < 0 {
@@ -40,10 +47,15 @@ func main() {
             currentPosition = currentPosition - 100
         }
 
-        fmt.Println(currentPosition)
+        if currentPosition < 0 || currentPosition > 99 {
+            log.Panic("wtff")
+        }
+
+        // fmt.Println(currentPosition)
         if currentPosition == 0 {
             timesAtZero += 1
         }
+
     }
 
     if err := scanner.Err(); err != nil {
